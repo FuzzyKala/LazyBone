d39d59cfe7c28712e62401001d5fe0ca458dd3f5
fuzzykala <tangerinekey381@gmail.com>
2025-02-21 11:54:12 +0200

    before merge to main

--- app/src/main/java/com/example/lazybone/main/api/ExerciseRepository.kt
+++ app/src/main/java/com/example/lazybone/main/api/ExerciseRepository.kt
@@ -3,7 +3,7 @@ package com.example.lazybone.main.api
 import android.util.Log
 
 
-//Acts as a middle layer to abstract away the details of making API calls and managing data.
+// Acts as a middle layer to abstract away the details of making API calls and managing data.
 class ExerciseRepository(private val api: ExerciseApiService) {
 
     suspend fun fetchExercises(bodyPart: String, bodyParts: List<BodyPart>): List<Exercise>? {
